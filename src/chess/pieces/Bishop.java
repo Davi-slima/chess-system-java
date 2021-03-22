05b168c5947ab3b05724b30543fe392f347e350b
Davi dos Santos <davi.slima75@gmail.com>
2021-03-22 17:00:16 -0300

    Bishop

---
+++ src/chess/pieces/Bishop.java
@@ -0,0 +1,77 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Bishop extends ChessPiece {
+
+	public Bishop(Board board, Color color) {
+		super(board, color);
+	}
+
+	@Override
+	public String toString() {
+		return "B";
+	}
+	
+//	DEFINI플O DOS MOVIMENTOS DO BISPO: 
+	
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position p = new Position(0,0);
+		
+		// MOVIMENTOS PARA A DIAGONAL NW:
+		
+				p.setValues(position.getRow() - 1, position.getColumn() - 1);
+				
+				while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { // ESSA OEPRA플O CONTINAR� A SE REPETIR ENQUANTO HOUVER CASAS VAZIAS 
+					mat[p.getRow()][p.getColumn()] = true;
+					p.setValues(p.getRow() - 1, p.getColumn() - 1);
+				}
+				if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+					mat[p.getRow()][p.getColumn()] = true;
+				}
+				
+				// MOVIMENTOS PARA A DIAGONAL NE:
+				
+				p.setValues(position.getRow() - 1, position.getColumn() + 1);
+				
+				while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { 
+					mat[p.getRow()][p.getColumn()] = true;
+					p.setValues(p.getRow() - 1, p.getColumn() + 1);
+				}
+				if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+					mat[p.getRow()][p.getColumn()] = true;
+				}
+				
+				// MOVIMENTOS PARA A DIAGONAL SE:
+				
+				p.setValues(position.getRow() + 1, position.getColumn() + 1);
+				
+				while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {  
+					mat[p.getRow()][p.getColumn()] = true;
+					p.setValues(p.getRow() + 1, p.getColumn() + 1);
+				}
+				if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+					mat[p.getRow()][p.getColumn()] = true;
+				}
+				
+				// MOVIMENTOS PARA A DIAGONAL SW:
+				
+				p.setValues(position.getRow() + 1, position.getColumn() - 1);
+				while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { // ESSA OEPRA플O CONTINAR� A SE REPETIR ENQUANTO HOUVER CASAS VAZIAS 
+					mat[p.getRow()][p.getColumn()] = true;
+					p.setValues(p.getRow() + 1, p.getColumn() - 1);
+				}
+				if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+					mat[p.getRow()][p.getColumn()] = true;
+				}
+		
+		return mat;
+	}
+
+}
