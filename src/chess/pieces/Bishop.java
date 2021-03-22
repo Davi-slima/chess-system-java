8cae3d2de981fc9c63c992bc9553ed571514fef8
Davi dos Santos <davi.slima75@gmail.com>
2021-03-22 17:45:40 -0300

    Queen

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -28,7 +28,7 @@ public class Bishop extends ChessPiece {
 		
 				p.setValues(position.getRow() - 1, position.getColumn() - 1);
 				
-				while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { // ESSA OEPRA플O CONTINAR� A SE REPETIR ENQUANTO HOUVER CASAS VAZIAS 
+				while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { 
 					mat[p.getRow()][p.getColumn()] = true;
 					p.setValues(p.getRow() - 1, p.getColumn() - 1);
 				}
@@ -63,7 +63,7 @@ public class Bishop extends ChessPiece {
 				// MOVIMENTOS PARA A DIAGONAL SW:
 				
 				p.setValues(position.getRow() + 1, position.getColumn() - 1);
-				while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { // ESSA OEPRA플O CONTINAR� A SE REPETIR ENQUANTO HOUVER CASAS VAZIAS 
+				while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { 
 					mat[p.getRow()][p.getColumn()] = true;
 					p.setValues(p.getRow() + 1, p.getColumn() - 1);
 				}
